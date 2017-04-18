74518f449c7488d98a32bca96ab1871adf8752d2
Allen Boynton <aboynton@fullsail.edu>
2017-04-17 23:08:17 -0400

    Segue from 1st VC and created web view
    Segued title from selected row…want to segue image too. Set up web view
    to play video from youTube.

--- PartyRock/PartyRock/MainVC.swift
+++ PartyRock/PartyRock/MainVC.swift
@@ -28,7 +28,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         let p2 = PartyRock(imageURL: "http://www.croshalgroup.com/wp-content/uploads/2015/05/Redfoo-Website.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/1w9DiGlZksU\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Let's Get Ridiculous")
 
-        let p3 = PartyRock(imageURL: "http://i.ytimg.com/vi/2wUxw6GH3IM/hqdefault.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/vg_nvEGryA4\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Juicy Wiggle Lesson")
+        let p3 = PartyRock(imageURL: "http://i.dailymail.co.uk/i/pix/2015/02/25/260ECA1D00000578-2967882-Sexy_ladies_Redfoo_returns_to_Australia_to_launch_his_latest_sin-a-14_1424829680373.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/vg_nvEGryA4\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Juicy Wiggle Lesson")
 
         let p4 = PartyRock(imageURL: "http://www.billboard.com/files/styles/article_main_image/public/media/lmfao-party-rock-anthem-2011-billboard-650.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/gZIqW92GaTQ\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Party Rock Commercial")
 
@@ -54,9 +54,23 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         return partyRocks.count
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let partyRock = partyRocks[indexPath.row]
+        
+        performSegue(withIdentifier: segueToVC, sender: partyRock)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? VideoVC {
+            
+            if let party = sender as? PartyRock {
+                destination.partyRock = party
+//                destination.musicImage.image = party.imageURL
+            }
+        }
+    }
 }
 
--- PartyRock/PartyRock/VideoVC.swift
+++ PartyRock/PartyRock/VideoVC.swift
@@ -10,15 +10,25 @@ import UIKit
 
 class VideoVC: UIViewController {
     
-    
+    @IBOutlet weak var webView: UIWebView!
 
+    @IBOutlet weak var videoTitle: UILabel!
+        
+    private var _partyRock: PartyRock!
+    
+    var partyRock: PartyRock {
+        get {
+            return _partyRock
+        }
+        set {
+            _partyRock = newValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    }
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        videoTitle.text = partyRock.videoTitle
+        webView.loadHTMLString(partyRock.videoURL, baseURL: nil)
     }
 }
