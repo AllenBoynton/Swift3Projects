974f61cd41abf96f6fdf803f8918de80f09f6116
Allen Boynton <aboynton@fullsail.edu>
2017-04-17 21:52:47 -0400

    Initial commit
    Added UI, set up table view and initiated protocols for url images,
    video and titles.

---
+++ PartyRock/PartyRock/PartyCell.swift
@@ -0,0 +1,45 @@
+//
+//  PartyCell.swift
+//  PartyRock
+//
+//  Created by Allen Boynton on 4/17/17.
+//  Copyright © 2017 ABtech Applications. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+    
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    
+    @IBOutlet weak var videoTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func updateUI(partyRock: PartyRock) {
+        
+        // Creates title in table view row
+        videoTitle.text = partyRock.videoTitle
+        
+        // Run asyc - get pic from web
+        let url = URL(string: partyRock.imageURL)!
+        
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+            }
+            catch {
+                
+            }
+        }
+        
+    }
+
+    
+}
---
+++ PartyRock/PartyRock/VideoVC.swift
@@ -0,0 +1,24 @@
+//
+//  VideoVC.swift
+//  PartyRock
+//
+//  Created by Allen Boynton on 4/17/17.
+//  Copyright © 2017 ABtech Applications. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
