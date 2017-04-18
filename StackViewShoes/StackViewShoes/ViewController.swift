f81b42938297ab8a289ced120c720518c46c8947
Allen Boynton <aboynton@fullsail.edu>
2017-04-17 21:50:15 -0400

    Initial commit
    Added UI, added images, put in vertical and horizontal stack views and
    used equal spacing to adjust for looks.

---
+++ StackViewShoes/StackViewShoes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewShoes
+//
+//  Created by Allen Boynton on 4/14/17.
+//  Copyright © 2017 ABtech Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
