10355d5828ab8f295a3d6c70bae8cb35148d030c
Allen Boynton <alboynton4@gmail.com>
2017-06-27 22:01:44 -0400

    Commit Notif push project to repo

---
+++ PushMyNotifs/PushMyNotifs/AppDelegate.swift
@@ -0,0 +1,118 @@
+//
+//  AppDelegate.swift
+//  PushMyNotifs
+//
+//  Created by Allen Boynton on 6/15/17.
+//  Copyright © 2017 Allen Boynton. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseInstanceID
+import FirebaseMessaging
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    func connectToFCM() {
+        Messaging.messaging().shouldEstablishDirectChannel = true { (error) in
+            if (error != nil) {
+                print("Unable to connect to FCM \(error)")
+            } else {
+                print("Connected to FCM")
+            }
+        }
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    // Registering for Firebase notifications
+    func configureFirebase(application: UIApplication) {
+        
+        FirebaseApp.configure()
+        
+        Messaging.messaging().delegate = self
+        
+        // Register for remote notifications. This shows a permission dialog on first run, to
+        // show the dialog at a more appropriate time move this registration accordingly.
+        // [START register_for_notifications]
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            userNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            userNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        
+        
+        print("-----firebase token: \(String(describing: Messaging.messaging().fcmToken)) ----")
+        
+        
+    }
+    
+    
+    //MARK: FCM Token Refreshed
+    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+        // FCM token updated, update it on Backend Server
+    }
+    
+    
+    func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+        print("remoteMessage: \(remoteMessage)")
+    }
+    
+    //Called when a notification is delivered to a foreground app.
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    //Called to let your app know which action was selected by the user for a given notification.
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("User Info = \(response.notification.request.content.userInfo)")
+        
+        completionHandler()
+    }
+}
---
+++ PushMyNotifs/PushMyNotifs/MainNotifVC.swift
@@ -0,0 +1,22 @@
+//
+//  MainNotifVC.swift
+//  PushMyNotifs
+//
+//  Created by Allen Boynton on 6/15/17.
+//  Copyright © 2017 Allen Boynton. All rights reserved.
+//
+
+import UIKit
+
+class MainNotifVC: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+}
+
